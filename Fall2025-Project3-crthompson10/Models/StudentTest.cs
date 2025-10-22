4daf92a44781ee9f74bc1a2091de207cdd0081a2
crthompson10 <crthompson10@crimson.ua.edu>
2025-10-22 18:09:21 -0500

    Initial Commit

---
+++ Fall2025-Project3-crthompson10/Models/StudentTest.cs
@@ -0,0 +1,9 @@
+namespace Fall2025_Project3_crthompson10.Models;
+
+public class StudentTest
+{
+    public int ID {get; set;}
+    public string Name {get; set;}
+    public string? Major {get; set;}
+    public DateTime DOB {get; set;}
+}
